0bb57bafc3982b2ba88d145b76471790ae6d7ab3
Mérovée Charrue <merovee@charrue.org>
2021-02-11 21:58:24 +0100

    chore: change base namespace

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Friendeals;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
